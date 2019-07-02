0fd6e5d8e8b1be2a303b145c395b7172af2d9002
Thomas Jensen <thomasjensen@Thomass-MacBook-Pro.local>
2019-07-02 18:41:42 -0500

    added charecters

--- app/data/friends.js
+++ app/data/friends.js
@@ -13,8 +13,8 @@ var friends = [
   
     },
     {
-        name: "Luke Skywalker",
-        photo: "https://fsmedia.imgix.net/52/39/b7/4f/9e53/49ec/ba45/8ed4b92c1c66/is-luke-skywalker-alive-in-episode-ix.jpeg",
+        name: "Yoda",
+        photo: "https://spacequotations.com/wp-content/uploads/2018/09/yoda-810x456.jpg",
         score:[5,4,3,2,1,5,4,3,2,1]
   
     },
