dc4a3ff74a97122008c4abe751286c64a6b2dcf4
Thomas Jensen <thomasjensen@Thomass-MacBook-Pro.local>
2019-07-01 20:20:56 -0500

    made friends objects on friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,47 @@
+var friends = [
+    {
+      name: "Luke Skywalker",
+      photo: "https://fsmedia.imgix.net/52/39/b7/4f/9e53/49ec/ba45/8ed4b92c1c66/is-luke-skywalker-alive-in-episode-ix.jpeg",
+      score:[5,4,3,2,1,5,4,3,2,1]
+
+    },
+    
+    {
+        name: "Luke Skywalker",
+        photo: "https://fsmedia.imgix.net/52/39/b7/4f/9e53/49ec/ba45/8ed4b92c1c66/is-luke-skywalker-alive-in-episode-ix.jpeg",
+        score:[5,4,3,2,1,5,4,3,2,1]
+  
+    },
+    {
+        name: "Luke Skywalker",
+        photo: "https://fsmedia.imgix.net/52/39/b7/4f/9e53/49ec/ba45/8ed4b92c1c66/is-luke-skywalker-alive-in-episode-ix.jpeg",
+        score:[5,4,3,2,1,5,4,3,2,1]
+  
+    },
+    {
+        name: "Luke Skywalker",
+        photo: "https://fsmedia.imgix.net/52/39/b7/4f/9e53/49ec/ba45/8ed4b92c1c66/is-luke-skywalker-alive-in-episode-ix.jpeg",
+        score:[5,4,3,2,1,5,4,3,2,1]
+  
+    },
+    {
+        name: "Luke Skywalker",
+        photo: "https://fsmedia.imgix.net/52/39/b7/4f/9e53/49ec/ba45/8ed4b92c1c66/is-luke-skywalker-alive-in-episode-ix.jpeg",
+        score:[5,4,3,2,1,5,4,3,2,1]
+  
+    },
+    {
+        name: "Luke Skywalker",
+        photo: "https://fsmedia.imgix.net/52/39/b7/4f/9e53/49ec/ba45/8ed4b92c1c66/is-luke-skywalker-alive-in-episode-ix.jpeg",
+        score:[5,4,3,2,1,5,4,3,2,1]
+  
+    },
+    {
+        name: "Luke Skywalker",
+        photo: "https://fsmedia.imgix.net/52/39/b7/4f/9e53/49ec/ba45/8ed4b92c1c66/is-luke-skywalker-alive-in-episode-ix.jpeg",
+        score:[5,4,3,2,1,5,4,3,2,1]
+  
+    },
+]
+
+module.exports = friends;
