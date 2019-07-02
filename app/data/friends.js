25a72b766e28755a37cdfb717f08d1c694aaa2c0
Thomas Jensen <thomasjensen@Thomass-MacBook-Pro.local>
2019-07-01 20:29:55 -0500

    started adding ddifferent profiles on friends page

--- app/data/friends.js
+++ app/data/friends.js
@@ -7,9 +7,9 @@ var friends = [
     },
     
     {
-        name: "Luke Skywalker",
-        photo: "https://fsmedia.imgix.net/52/39/b7/4f/9e53/49ec/ba45/8ed4b92c1c66/is-luke-skywalker-alive-in-episode-ix.jpeg",
-        score:[5,4,3,2,1,5,4,3,2,1]
+        name: "Boba Fett",
+        photo: "https://s.yimg.com/ny/api/res/1.2/nWUDePrLZ0yqnfLlBAk6HA--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAwO2lsPXBsYW5l/http://media.zenfs.com/en_US/News/BGR_News/star-wars-square-enix-boba-fett-1.jpg",
+        score:[5,5,4,4,3,3,4,4,5,5]
   
     },
     {
